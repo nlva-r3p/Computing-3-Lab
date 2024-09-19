02434446ac329faa38a79e045fe902959f0631e0
DIEEEEEEEEEEEE <Brandon_Bui1@student.uml.edu>
2024-09-19 15:30:29 -0400

    Create lab2.cpp

---
+++ lab2.cpp
@@ -0,0 +1,32 @@
+/*
+Presenter: Nguyen Mai, clarified questions/answers to problems we ran into during the coding process.
+Manager: Brandon Bui, I made sure we were working on separate parts of the lab and figuring it out coding wise before we put it all
+together.
+Reflector: Umit Nazar, made sure everyone was working on what they needed to be and that we all understood what we were brainstorming.
+Recorder: Aayush Patel, typed down each of our ideas as well as general names for the variables and/or programs we planned to code.
+*/
+
+#include <iostream>
+using namespace std;
+
+void getWeight1(int lbs, double oz);
+void conversion(int lbs, double oz, int kg, double g);
+
+
+
+int main(int argc, char* argv[])
+{
+	int pounds;
+	double ounces;
+	int kilograms;
+	double grams;
+
+
+}
+
+
+void conversion(int lbs, double oz, int& kg, double& g)
+{
+
+
+}
