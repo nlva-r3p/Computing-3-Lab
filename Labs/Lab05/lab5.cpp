33d9b57c6ba2ed191cd4dfb6b76ba9a21db7c5b1
DIEEEEEEEEEEEE <Brandon_Bui1@student.uml.edu>
2024-10-16 16:52:10 -0400

    Update lab5.cpp

--- Labs/Lab05/lab5.cpp
+++ Labs/Lab05/lab5.cpp
@@ -1,4 +1,41 @@
 #include <iostream>
 
-int main (int argc, int *argv[]) {
+using namespace std;
+
+class ComplexNumber
+{
+	
+public:
+	ComplexNumber() : real(0), imagine(0) {}
+
+	ComplexNumber(int r, int i)
+	{
+		setNumbers(r, i);
+	}
+
+	void setNumbers(int r, int i);
+	void outputNumbers();
+
+private:
+	int real;
+	int imagine;
+
+};
+
+void ComplexNumber::setNumbers(int r, int i)
+{
+	real = r;
+	imagine = i;
+}
+
+void ComplexNumber::outputNumbers()
+{
+	cout << "c1.Re() == " << real << ", c1.Im() == " << imagine;
+
+}
+
+int main(int argc, int* argv[]) 
+{
+
+
 }
