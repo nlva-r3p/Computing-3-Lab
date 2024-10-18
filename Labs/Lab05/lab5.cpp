0d34a455957f0050d68ad97c5b8769fb8e443e61
aPatel2975 <160794781+aPatel2975@users.noreply.github.com>
2024-10-17 21:07:51 -0400

    Update lab5.cpp

--- Labs/Lab05/lab5.cpp
+++ Labs/Lab05/lab5.cpp
@@ -1,133 +1,134 @@
 #include <iostream>
+#include <vector>
 
-class ComplexNumber {	
+class ComplexNumber {
 public:
+    // Constructors
+    ComplexNumber() : real(0), imaginary(0) {}
+    ComplexNumber(double r, double i) : real(r), imaginary(i) {}
 
-	// constructors
-	ComplexNumber() : real(0), imaginary(0) {}
-	ComplexNumber(double r = 0.0, double i = 0.0) : real(r), imaginary(i) {}
+    // Mutators
+    void setReal(double r);
+    void setImaginary(double i);
+    void setNumbers(double r, double i);
 
-	// mutators
-	void setReal(double r); 
-	void setImaginary(double i);
-	void setNumbers(double r, double i);
+    // Accessors
+    double getReal() const;
+    double getImaginary() const;
+    void outputNumbers() const;
 
-	// accessors
-	double getReal() const;
-	double getImaginary() const;
-	void outputNumbers() const;
+    // Operator overloads
+    friend std::ostream& operator<<(std::ostream& os, const ComplexNumber& number);
 
-	friend std::ostream& operator<<(std::ostream& os, const ComplexNumber& number);
-
-	ComplexNumber operator+(const ComplexNumber& other) const;
-	ComplexNumber operator-(const ComplexNumber& other) const;
-	ComplexNumber operator*(const ComplexNumber& other) const;
-	ComplexNumber operator/(const ComplexNumber& other) const;
-	ComplexNumber operator!() const;
+    ComplexNumber operator+(const ComplexNumber& other) const;
+    ComplexNumber operator-(const ComplexNumber& other) const;
+    ComplexNumber operator*(const ComplexNumber& other) const;
+    ComplexNumber operator/(const ComplexNumber& other) const;
+    ComplexNumber operator!() const;
 
 private:
-	double real;
-	double imaginary;
+    double real;
+    double imaginary;
 };
 
+// Mutator definitions
 void ComplexNumber::setReal(double r) {
-	real = r;
+    real = r;
 }
 
 void ComplexNumber::setImaginary(double i) {
-	imaginary = i;
+    imaginary = i;
 }
 
 void ComplexNumber::setNumbers(double r, double i) {
-	real = r;
-	imaginary = i;
+    real = r;
+    imaginary = i;
 }
 
-double ComplexNumber::getReal() {
-	return real;
+// Accessor definitions
+double ComplexNumber::getReal() const {
+    return real;
 }
 
-double ComplexNumber::getImaginary() {
-	return imaginary;
+double ComplexNumber::getImaginary() const {
+    return imaginary;
 }
 
-void ComplexNumber::outputNumbers() {
-	cout << "c1.Re() == " << real << ", c1.Im() == " << imaginary;
+void ComplexNumber::outputNumbers() const {
+    std::cout << "Re == " << real << ", Im == " << imaginary << std::endl;
 }
 
+// Operator overload definitions
 std::ostream& operator<<(std::ostream& os, const ComplexNumber& number) {
-	os << number.real;
-	if (number.imaginary < 0)
-		os << number.imaginary;
-	else
-		os << '+' << number.imaginary;
-	os << 'i' << endl;
-
-	return os;
+    os << number.real;
+    if (number.imaginary >= 0)
+        os << '+';
+    os << number.imaginary << 'i';
+    return os;
 }
 
-ComplexNumber operator+(const ComplexNumber& other) const {
-	return ComplexNumber(real + other.real, imaginary + other.imaginary);
+ComplexNumber ComplexNumber::operator+(const ComplexNumber& other) const {
+    return ComplexNumber(real + other.real, imaginary + other.imaginary);
 }
 
-ComplexNumber operator-(const ComplexNumber& other) const {
-	return ComplexNumber(real - other.real, imaginary - other.imaginary);
+ComplexNumber ComplexNumber::operator-(const ComplexNumber& other) const {
+    return ComplexNumber(real - other.real, imaginary - other.imaginary);
 }
 
-ComplexNumber operator*(const ComplexNumber& other) const {
-	double newReal = real * other.real - imaginary * other.imaginary;
-	double newImaginary = real * other.imaginary + imaginary * other.real;
-	return ComplexNumber(newReal, newImaginary);
+ComplexNumber ComplexNumber::operator*(const ComplexNumber& other) const {
+    double newReal = real * other.real - imaginary * other.imaginary;
+    double newImaginary = real * other.imaginary + imaginary * other.real;
+    return ComplexNumber(newReal, newImaginary);
 }
 
-ComplexNumber operator/(const ComplexNumber& other) const {
-	double denominator = other.real * other.real + other.imaginary * other.imaginary;
-	double newReal = (real * other.real + imaginary * other.imaginary) / denominator;
-	double newImaginary = (imaginary * other.real - real * other.imaginary) / denominator;
-	return ComplexNumber(newReal, newImaginary);
+ComplexNumber ComplexNumber::operator/(const ComplexNumber& other) const {
+    double denominator = other.real * other.real + other.imaginary * other.imaginary;
+    double newReal = (real * other.real + imaginary * other.imaginary) / denominator;
+    double newImaginary = (imaginary * other.real - real * other.imaginary) / denominator;
+    return ComplexNumber(newReal, newImaginary);
 }
 
-ComplexNumber operator!() const {
-	return ComplexNumber(real, -imaginary);
+ComplexNumber ComplexNumber::operator!() const {
+    return ComplexNumber(real, -imaginary);
 }
 
-int main(int argc, int* argv[]) 
-{
-	ComplexNumber c1;
-	ComplexNumber c2(1, 2);
-	ComplexNumber c3(3, 4);
+// Main function
+int main() {
+    ComplexNumber c1; // Default constructor
+    ComplexNumber c2(1, 2); // Value constructor
+    ComplexNumber c3(3, 4);
 
-	cout << "Value of c1 (default constructor):" << endl;
-	cout << "c1.Re() == " << c1.getReal() << ", c1.Im() == " << c1.getImaginary() << endl;
+    std::cout << "Value of c1 (default constructor):" << std::endl;
+    std::cout << "c1.Re() == " << c1.getReal() << ", c1.Im() == " << c1.getImaginary() << std::endl;
 
-	cout << "Value of c2 (default constructor):" << endl;
-	cout << "c2.Re() == " << c2.getReal() << ", c2.Im() == " << c2.getImaginary() << endl;
-	cout << endl;
+    std::cout << "Value of c2 (value constructor):" << std::endl;
+    std::cout << "c2.Re() == " << c2.getReal() << ", c2.Im() == " << c2.getImaginary() << std::endl;
 
-    	cout << "c2 + c3 (" << c2 << " + " << c3 << ") == " << (c2 + c3) << endl;
-    	cout << "c2 - c3 (" << c2 << " - " << c3 << ") == " << (c2 - c3) << endl;
-    	cout << "c2 * c3 (" << c2 << " * " << c3 << ") == " << (c2 * c3) << endl;
-   	cout << "c2 / c3 (" << c2 << " / " << c3 << ") == " << (c2 / c3) << endl;
+    // Test arithmetic operations
+    std::cout << "c2 + c3 (" << c2 << " + " << c3 << ") == " << (c2 + c3) << std::endl;
+    std::cout << "c2 - c3 (" << c2 << " - " << c3 << ") == " << (c2 - c3) << std::endl;
+    std::cout << "c2 * c3 (" << c2 << " * " << c3 << ") == " << (c2 * c3) << std::endl;
+    std::cout << "c2 / c3 (" << c2 << " / " << c3 << ") == " << (c2 / c3) << std::endl;
 
-	cout << "Conjugate of c2 (" << c2 << ") == " << !c2 << endl; 
-	cout << endl;
+    // Test complex conjugate
+    std::cout << "Conjugate of c2 (" << c2 << ") == " << !c2 << std::endl;
 
-	vector<ComplexNumber> vec;
-	for (int n = 0; n < 10; ++n) {
-		vec.push_back(ComplexNumber(n, 2 * n));
-	}
+    // Vector of complex numbers
+    std::vector<ComplexNumber> vec;
+    for (int n = 0; n < 10; ++n) {
+        vec.push_back(ComplexNumber(n, 2 * n));
+    }
 
+    std::cout << "Vector output using indexed for-loop:" << std::endl;
+    for (int i = 0; i < 10; ++i) {
+        std::cout << vec[i] << std::endl;
+    }
 
-	cout << "Vector output using indexed for-loop:" << endl;
-	for (int i = 0; i < 10; ++i) {
-		cout << vec[i] << endl;
-	}
-	cout << endl;
+    std::cout << "Vector output using iterator for-loop:" << std::endl;
+    for (auto it = vec.begin(); it != vec.end(); ++it) {
+        std::cout << *it << std::endl;
+    }
 
-	
-	cout << "Vector output using iterator for-loop:" << endl;
-	for ( auto it = vec.begin(); it != vec.end(); ++it) {
-		cout << *it << endl;
-	}
-
+    return 0;
 }
+
